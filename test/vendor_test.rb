805f01a996a3e61952c187c106b5402251542f6d
Gunnar <dunkcrunk.com>
2021-03-10 11:09:41 -0700

    Add iteration 2

--- test/vendor_test.rb
+++ test/vendor_test.rb
@@ -4,4 +4,52 @@ require './lib/item'
 require './lib/vendor'
 
 class VendorTest < Minitest::Test
+    def test_it_exists
+    item1 = Item.new({name: 'Peach', price: "$0.75"})
+    item2 = Item.new({name: 'Tomato', price: '$0.50'})
+    vendor = Vendor.new("Rocky Mountain Fresh")
+    assert_instance_of Vendor, vendor
+  end
+
+  def test_it_has_attributes
+    item1 = Item.new({name: 'Peach', price: "$0.75"})
+    item2 = Item.new({name: 'Tomato', price: '$0.50'})
+    vendor = Vendor.new("Rocky Mountain Fresh")
+    assert_equal "Rocky Mountain Fresh", vendor.name
+    assert_equal ({}), vendor.inventory
+  end
+
+  def test_it_can_check_inventory
+    item1 = Item.new({name: 'Peach', price: "$0.75"})
+    item2 = Item.new({name: 'Tomato', price: '$0.50'})
+    vendor = Vendor.new("Rocky Mountain Fresh")
+    assert_equal 0, vendor.check_stock(item1)
+  end
+
+  def test_it_can_restock
+    item1 = Item.new({name: 'Peach', price: "$0.75"})
+    item2 = Item.new({name: 'Tomato', price: '$0.50'})
+    vendor = Vendor.new("Rocky Mountain Fresh")
+    vendor.stock(item1, 30)
+    assert_equal ({item1 => 30}), vendor.inventory
+  end
+
+  def test_it_can_restock_an_item_twice
+    item1 = Item.new({name: 'Peach', price: "$0.75"})
+    item2 = Item.new({name: 'Tomato', price: '$0.50'})
+    vendor = Vendor.new("Rocky Mountain Fresh")
+    vendor.stock(item1, 30)
+    vendor.stock(item1, 25)
+    assert_equal 55, vendor.check_stock(item1)
+  end
+
+  def test_it_can_restock_two_items
+    item1 = Item.new({name: 'Peach', price: "$0.75"})
+    item2 = Item.new({name: 'Tomato', price: '$0.50'})
+    vendor = Vendor.new("Rocky Mountain Fresh")
+    vendor.stock(item1, 30)
+    vendor.stock(item1, 25)
+    vendor.stock(item2, 12)
+    assert_equal ({item1 => 55, item2 => 12}), vendor.inventory
+  end
 end
